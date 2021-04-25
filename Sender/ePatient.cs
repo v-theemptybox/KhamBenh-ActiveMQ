cb876f0f09db4d55dbb42a3dc95f3ff31b04e634
Vinh Nguyen <vinhnguyen.goid2@gmail.com>
2021-04-26 01:39:41 +0700

    Add Patient entity

---
+++ Sender/ePatient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class ePatient
+    {
+        public string Id { get; set; }
+        public string PId { get; set; }
+        public string FName { get; set; }
+        public string Address { get; set; }
+        public ePatient(string msbn, string socmnd, string hoten, string diachi)
+        {
+            this.Id = msbn;
+            this.PId = socmnd;
+            this.FName = hoten;
+            this.Address = diachi;
+
+        }
+        public ePatient()
+        {
+
+        }
+    }
+}
